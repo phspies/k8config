defdba155da41aeef7a9efe47341bd84ae3450e8
Phillip Spies <fspies0@hotmail.com>
2021-07-24 22:44:19 -0400

    first commit

---
+++ k8config/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace k8config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
